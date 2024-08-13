4bb5a7b987306d5c4a202d5720ad4c87e5f5bff5
alxgrn <alxgrn@users.noreply.github.com>
2024-08-13 16:13:50 +0300

    Подправлен экспорт типов

--- lib/index.ts
+++ lib/index.ts
@@ -1,9 +1,37 @@
 import Notes from "./Notes/Notes";
 import Editor from "./Editor/Editor";
 import Viewer from "./Viewer/Viewer";
+import EditorHeader from "./components/EditorHeader";
 
 export {
     Notes,
     Editor,
     Viewer,
+    EditorHeader,
+};
+
+import {
+    TArticle,
+    TArticleType,
+    TComment,
+    TCreatePublication,
+    TEditorSaver,
+    TImageUploader,
+    TNotesSaver,
+    TPublication,
+    TPublicationFormat,
+    TUpdatePublication,
+} from "./types";
+
+export type {
+    TArticle,
+    TArticleType,
+    TComment,
+    TCreatePublication,
+    TEditorSaver,
+    TImageUploader,
+    TNotesSaver,
+    TPublication,
+    TPublicationFormat,
+    TUpdatePublication,
 };
