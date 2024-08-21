eb0297942bb76c30d88854aa9283f448764fe256
alxgrn <alxgrn@users.noreply.github.com>
2024-08-21 09:41:24 +0300

    Флаги у Viewer

--- lib/Viewer/Viewer.tsx
+++ lib/Viewer/Viewer.tsx
@@ -3,12 +3,14 @@ import { TArticle, TComment } from "../types";
 import QuillViewer from "../Quill/QuillViewer";
 
 type Props = {
+    short?: boolean; // флаг варианта набора тегов у контента - полный или компактный
     article: TArticle | TComment;
+    expandable?: boolean; // флаг того надо ли выводить контент свернутым если он слишком длинный
 };
 
-const Viewer: FC<Props> = ({ article }) => {
+const Viewer: FC<Props> = ({ short, article, expandable }) => {
     if (article.format === 'delta') {
-        return <QuillViewer content={article.content}/>
+        return <QuillViewer content={article.content} short={short} expandable={expandable}/>
     }
 
     return <div>Неизвестный формат публикации</div>;
