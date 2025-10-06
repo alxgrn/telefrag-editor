965b1842be2d4dd65c4557d6c9e9c9121539099a
alxgrn <alxgrn@users.noreply.github.com>
2025-10-06 15:59:06 +0300

    Вынесен конфигуратор

--- lib/index.ts
+++ lib/index.ts
@@ -2,12 +2,14 @@ import Notes from "./Notes/Notes";
 import Editor from "./Editor/Editor";
 import Viewer from "./Viewer/Viewer";
 import EditorHeader from "./components/EditorHeader";
+import { setConfig } from "@alxgrn/prose-editor";
 
 export {
     Notes,
     Editor,
     Viewer,
     EditorHeader,
+    setConfig,
 };
 
 import {
