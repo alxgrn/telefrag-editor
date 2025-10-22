b056a80350110b92b0645506c8356258549103f2
alxgrn <alxgrn@users.noreply.github.com>
2025-10-22 20:53:12 +0300

    UI update

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,11 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { setConfig } from '@alxgrn/prose-editor'
+
+setConfig({
+  api_url: 'https://dailytelefrag.ru/api/files'
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   //<React.StrictMode>
